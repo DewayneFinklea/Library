7f448be6c3c4962ba414e50af55d021dea9a4daf
Dewayne Finklea <dfinklea328@gmail.com>
2023-03-10 11:40:19 -0600

    started getTotalNumberOfBorrows function -- incomplete

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -6,7 +6,11 @@ function sortAccountsByLastName(accounts) {
   return accounts.sort((accountA, accountB) => accountA.name.last > accountB.name.last ? 1 : -1);
 }
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+ /* let count = 0;
+  books.forEach(book => count += book.borrows.id.includes(account.id) ? 1 : 0);
+  return count;
+}*/
 
 function getBooksPossessedByAccount(account, books, authors) {}
 
