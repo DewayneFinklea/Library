c237cb026c6a0e5b7695348dbe3f25fc038c3645
Dewayne Finklea <dfinklea328@gmail.com>
2023-04-19 19:49:39 -0500

    completed src/accounts

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,18 +1,45 @@
+const { findAuthorById } = require("./books");
+
 function findAccountById(accounts, id) {
   return accounts.find((account) => account.id === id);
 }
 
 function sortAccountsByLastName(accounts) {
-  return accounts.sort((accountA, accountB) => accountA.name.last > accountB.name.last ? 1 : -1);
+  return accounts.sort((accountA, accountB) =>
+    accountA.name.last > accountB.name.last ? 1 : -1
+  );
 }
 
 function getTotalNumberOfBorrows(account, books) {
- /* let count = 0;
-  books.forEach(book => count += book.borrows.id.includes(account.id) ? 1 : 0);
+  let count = 0;
+  console.log(account);
+  books.forEach((book) => {
+    if (book.borrows)
+      book.borrows.forEach(
+        (borrower) => (count += borrower.id === account.id ? 1 : 0)
+      );
+  });
   return count;
-*/}
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  let checkedOut = books.filter((book) =>
+    book.borrows.some((borrow) => !borrow.returned && borrow.id === account.id)
+  );
+  const result = [];
+  checkedOut.forEach((book) => {
+    const bookAuthor = findAuthorById(authors, book.authorId);
+    result.push({
+      id: book.id,
+      title: book.title,
+      genre: book.genre,
+      authorId: book.authorId,
+      author: bookAuthor,
+      borrows: book.borrows,
+    });
+  });
+  return result;
+}
 
 module.exports = {
   findAccountById,
